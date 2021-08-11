02d7f21c38fdcb014a8b9dc5a37af6a9d43a4722
Kyle Bailey <kylealexbailey@gmail.com>
2021-08-11 23:57:12 +0900

    add CRUD actions

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,13 @@
 class Flat < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price_per_night,
+            presence: true,
+            numericality: { only_integer: true },
+            inclusion: { in: (10..1000) }
+  validates :number_of_guests,
+            presence: true,
+            numericality: { only_integer: true },
+            inclusion: { in: (1..10) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning your database..'
+Flat.destroy_all
+
+puts 'Creating flats..'
+12.times do
+  query = %w[apartment room house cottage home bedroom].sample
+  Flat.create!(
+    name: "#{Faker::Address.building_number} #{Faker::Address.community}",
+    address: Faker::Address.full_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(10..1000),
+    number_of_guests: rand(1..10),
+    img_url: "https://source.unsplash.com/featured/?#{query}"
+  )
+end
+puts 'Finished creating new flats!'
+
+# puts 'Adding reviews to restaurants..'
+# @restaurants = Restaurant.all
+# @restaurants.each do |restaurant|
+#   rand(5..10).times do
+#     review = Review.create!(
+#       content: Faker::Restaurant.review,
+#       rating: rand(0..5)
+#     )
+#     review.restaurant = restaurant
+#     review.save
+#   end
+# end
+
+# t.string :name
+# t.string :address
+# t.text :description
+# t.integer :price_per_night
+# t.integer :number_of_guests
